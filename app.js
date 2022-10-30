b70006a69d48fefa33be00d3daaca9da58c87ee8
chaejeongmin <ymsh3680@gmail.com>
2022-10-31 06:34:43 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+let a = 5; /* 기본적으로 const 사용, 필요할 때만 let 사용 */
+let b = 2;
+let myName = "jeongmin"; /* let -> 새로운 것을 만들 때 사용 */
+                         /* const -> 값이 절대로 변하지 않음 */
+                         /* var -> 절대 사용하지 않음 */
+console.log ( a * b) ;
+console.log ( a * b) ;
+console.log ( a / b) ;
+console.log ("hello " + myName);
+
+
+myName = "CJM"
+console.log ("your name is " + myName)
