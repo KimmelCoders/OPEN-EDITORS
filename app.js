dd538d765eec1bc72625ef2cac9a49b0db0f8223
chaejeongmin <ymsh3680@gmail.com>
2022-11-08 23:12:11 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-let a = 5; /* 기본적으로 const 사용, 필요할 때만 let 사용 */
-let b = 2;
-let myName = "jeongmin"; /* let -> 새로운 것을 만들 때 사용 */
-                         /* const -> 값이 절대로 변하지 않음 */
-                         /* var -> 절대 사용하지 않음 */
-console.log ( a * b) ;
-console.log ( a * b) ;
-console.log ( a / b) ;
-console.log ("hello " + myName);
+function sayHello(){
 
+}
 
-myName = "CJM"
-console.log ("your name is " + myName)
+const player = {
+    name: "Jeongmin",
+    sayHello: function (otherPersonsName) {
+        console.log("hello! " + otherPersonsName + " nice to meet you!");
+    },
+}
+
+console.log(player.name);
+player.sayHello("Yuri");
