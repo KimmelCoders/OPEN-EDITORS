87d75dc4b835275c302db9513a60fdb044e51c3d
chaejeongmin <ymsh3680@gmail.com>
2022-11-10 06:50:17 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-function sayHello(){
-
-}
-
-const player = {
-    name: "Jeongmin",
-    sayHello: function (otherPersonsName) {
-        console.log("hello! " + otherPersonsName + " nice to meet you!");
+const calculator = {
+    add: function(a, b){
+        console.log(a + b);
     },
-}
+    minus: function(a,b){
+        console.log(a - b);
+    },
+    div: function(a, b){
+        console.log(a/b);
+    },
+    powerof: function (a,b) {
+        console.log(a**b);
+    }
+};
 
-console.log(player.name);
-player.sayHello("Yuri");
+calculator.add(111,1);
+calculator.minus(100,1);
+calculator.div(100,5);
+calculator.powerof(2,7);
